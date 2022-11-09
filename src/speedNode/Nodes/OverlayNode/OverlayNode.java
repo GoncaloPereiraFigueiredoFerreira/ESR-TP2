fe4258529935d85520eba7a90b4602d616776d1f
HugoFerreiraNogueira <hugoferreiranogueira@gmail.com>
2022-11-09 22:18:39 +0000

    flood

--- src/speedNode/Nodes/OverlayNode/OverlayNode.java
+++ src/speedNode/Nodes/OverlayNode/OverlayNode.java
@@ -1,12 +1,14 @@
 package speedNode.Nodes.OverlayNode;
 
 import speedNode.Nodes.Serialize;
+import speedNode.Nodes.Tables.ClientTable;
+import speedNode.Nodes.Tables.NeighbourTable;
+import speedNode.Nodes.Tables.RoutingTable;
 import speedNode.TaggedConnection.Frame;
 import speedNode.TaggedConnection.TaggedConnection;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Arrays;
 import java.util.List;
 
 public class OverlayNode implements Runnable{
@@ -14,9 +16,21 @@ public class OverlayNode implements Runnable{
     private final int bootstrapPort = 12345;
     private final String bootstrapIP;
     TaggedConnection tc;
+    private final boolean server;
 
-    public OverlayNode(String bootstrapIP) {
+    private NeighbourTable neighbourTable;
+    private RoutingTable routingTable;
+    private ClientTable clientTable;
+
+    public OverlayNode(String bootstrapIP,boolean server,boolean client) {
+        this.server= server;
         this.bootstrapIP = bootstrapIP;
+        this.client= client;
+    }
+
+    private void initial_tables(List<String> ips){
+        this.neighbourTable.addNeighbours(ips);
+
     }
 
     public void run(){
@@ -28,11 +42,17 @@ public class OverlayNode implements Runnable{
 
             tc.send(0,1,b); //Send request with tag 1
             Frame frame = tc.receive();
-            List<String> ips = Serialize.deserialize(frame.getData());
+            List<String> ips = Serialize.deserializeListOfStrings(frame.getData());
             System.out.println(ips); //TODO - remover print
+            initial_tables(ips);
+            ControlWorker control_node= new ControlWorker(ips,this.server,this.neighbourTable,this.routingTable,this.clientTable);
+            control_node.run();
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public static void launch(List<String> args) {
@@ -40,7 +60,7 @@ public class OverlayNode implements Runnable{
             System.out.println("No arguments were given!");
             return;
         }
-        OverlayNode node = new OverlayNode(args.get(0));
+        OverlayNode node = new OverlayNode(args.get(0),args.get(1),args.get(2));
         node.run();
     }
 }
