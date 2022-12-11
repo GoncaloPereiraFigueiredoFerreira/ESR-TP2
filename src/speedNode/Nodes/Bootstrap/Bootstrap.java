6d5221432bf9f70556d9bf450712f8a4222c3ab7
HugoFerreiraNogueira <hugoferreiranogueira@gmail.com>
2022-12-11 21:13:11 +0000

    New readConfigFile

--- src/speedNode/Nodes/Bootstrap/Bootstrap.java
+++ src/speedNode/Nodes/Bootstrap/Bootstrap.java
@@ -3,16 +3,15 @@ package speedNode.Nodes.Bootstrap;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import speedNode.Utilities.Tuple;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Bootstrap implements Runnable{
     private ServerSocket ss;
@@ -23,7 +22,7 @@ public class Bootstrap implements Runnable{
     private BootstrapSharedInfo sharedInfo;
 
     public Bootstrap(String Bootstrap_config_filename) throws Exception {
-        sharedInfo = new BootstrapSharedInfo(readConfigFile(Bootstrap_config_filename));
+        sharedInfo = new BootstrapSharedInfo(readConfigFile_antigo(Bootstrap_config_filename));
     }
 
 
@@ -101,7 +100,7 @@ public class Bootstrap implements Runnable{
 
 
     // *************** Read Config .XML file ***************
-    public static Map<String, List<String>> readConfigFile(String filename) throws Exception {
+    public static Map<String, List<String>> readConfigFile_antigo(String filename) throws Exception {
 
         Map<String, List<String>> nodesMap = new HashMap<>();
 
@@ -139,6 +138,50 @@ public class Bootstrap implements Runnable{
             nodesMap.put(ip.getTextContent(), ips_vizinhos);
         }
 
+        return nodesMap;
+    }
+    public static Map<String, Tuple<Set<String>,Set<String>>> readConfigFile(String filename) throws Exception{
+
+        Map<String,Tuple<Set<String>,Set<String>>> nodesMap = new HashMap<>();
+        File file = new File(filename);
+
+        if (!file.exists())
+            throw new Exception("File '" + filename + "' not found!");
+
+        //an instance of factory that gives a document builder
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        //an instance of builder to parse the specified xml file
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(file);
+        doc.getDocumentElement().normalize();
+
+        NodeList nodeList = doc.getElementsByTagName("node");
+        for (int itr = 0; itr < nodeList.getLength(); itr++) {
+            Node node = nodeList.item(itr);
+
+            Node name;
+
+            Set<String> ips_interfaces = new HashSet<>();
+            Set<String> name_vizinhos = new HashSet<>();
+            var attributes = node.getAttributes();
+
+            if(attributes == null || (name= attributes.getNamedItem("name"))==null)
+                throw new Exception("Node needs to have an attribute \"name\".");
+
+            var vizinhos = node.getChildNodes();
+            for (int j = 0; j < vizinhos.getLength(); j++) {
+                Node vizinho = vizinhos.item(j);
+                if(vizinho != null)
+                    attributes = vizinho.getAttributes();
+                if (vizinho != null && vizinho.getNodeName().equals("interface"))
+                    ips_interfaces.add(attributes.getNamedItem("ip").getTextContent());
+                else if (vizinho != null && vizinho.getNodeName().equals("neighbour"))
+                    name_vizinhos.add(attributes.getNamedItem("name").getTextContent());
+            }
+            Tuple<Set<String>,Set<String>> tuple = new Tuple<>(ips_interfaces,name_vizinhos);
+            nodesMap.put(name.getTextContent(), tuple);
+        }
         return nodesMap;
     }
 }
